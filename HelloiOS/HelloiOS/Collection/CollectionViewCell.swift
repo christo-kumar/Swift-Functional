a1d138f9e880f3d5cf6b07e63b94f74bb07b9e08
christo <christoslimit@gmail.com>
2022-06-19 12:50:08 +0530

    Collection view working with image

---
+++ HelloiOS/HelloiOS/Collection/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  HelloiOS
+//
+//  Created by Christo Kumar on 19/06/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ HelloiOS/HelloiOS/Collection/CollectionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  CollectionViewController.swift
+//  HelloiOS
+//
+//  Created by Christo Kumar on 19/06/22.
+//
+
+import UIKit
+
+class CollectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCellID", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = UIImage(named: "CheckMark")
+        return cell
+    }
+    
+    
+}
