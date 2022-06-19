7f0d22880725252aba4d47a9158139716c372caf
christo <christoslimit@gmail.com>
2022-06-19 06:58:02 +0530

    Table View added

--- HelloiOS/HelloiOS/ViewController.swift
+++ HelloiOS/HelloiOS/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let rowData = ["Collect View", "Table View", "Custom Navigation", "Search Controller", "Scroll View","POP","Core Data", "Realm"
+    ]
 
+    func setupView() {
+        navigationItem.title = "iOS Controls"
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.setupView()
         // Do any additional setup after loading the view.
     }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.rowData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = rowData[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    
+}
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
 }
 
